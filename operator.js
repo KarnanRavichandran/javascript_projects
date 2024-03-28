cfe6a6b9b9bd8139d3fe5e800aef213bf1161feb
karnan <rkarnanr2018@gmail.com>
2024-03-28 12:03:28 +0530

    test

--- operator.js
+++ operator.js
@@ -1,18 +1,57 @@
 // assignment operator
-let a =50;
+// let a =50;
  
-a+= 5;
-console.log(a)
+// a+= 5;
+// console.log(a)
 
-a-= 5;
-console.log(a)
+// a-= 5;
+// console.log(a)
 
-a*= 5;
-console.log(a)
+// a*= 5;
+// console.log(a)
 
-a/= 5;
-console.log(a)
+// a/= 5;
+// console.log(a)
 
 
-a%= 5;
-console.log(a)
+// a%= 5;
+// console.log(a)
+
+
+// Comparsion operator
+
+// let a = 100;
+// let b = 100;
+// console.log(a===b)
+// console.log(a!=b)
+// == check with values
+// === check value with type of data
+
+
+// relation opertor
+/*
+>= =>greater than or equal
+<= less thn or equal
+
+*/
+
+// let a = 200;
+// let b = 120;
+// console.log(a >=b)
+
+// Logical opeartor in js
+// Logical AND
+// Logical OR
+// Logical NOT
+
+
+// AND
+// student pass or not
+//  35 - 100
+// let mark = 20;
+// console.log( mark >=35 && mark <=100)
+
+// OR
+// let mark = 20;
+// let total = 500;
+// console.log(mark ==35 || total == 500 )
