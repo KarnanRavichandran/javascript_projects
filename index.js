bf3635cd5530b10097ec99ec086ec482220c8cb3
karnan <rkarnanr2018@gmail.com>
2024-03-28 10:47:22 +0530

    console print

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // print name --- step => 1
 console.log('karnan')
+console.log(1213444)
+console.log(18.25)
+console.log(true)
+console.log(18.25)
+console.log([58,78,96,23])
+console.log({fname:"karnan"})
