cc7afecfaa215f34e0b12c1ed9c80619e0e8f134
karnan <rkarnanr2018@gmail.com>
2024-03-28 11:20:28 +0530

    type converstion

---
+++ dataType.js
@@ -0,0 +1,26 @@
+/*
+DATA TYPES
+
+2-ways
+
+1)prmitive datatype =>number,string,null,undefind,boolean,symbol
+2)refernace datatype =>array,object,data/time
+*/
+
+// Type conversin =>what type you will be define
+// number to strimg
+let a = 10;
+console.log(a,typeof a)
+a = String(a)
+console.log(a,typeof a)
+
+// string to number
+a = '1254'
+console.log(a,typeof a)
+a = Number(a)
+console.log(a,typeof a)
+
+a = '1254'
+console.log(a,typeof a)
+a = parseInt(a)
+console.log(a,typeof a)
