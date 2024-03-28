54e174f3685f025c013673eb0593cdc679d1c973
karnan <rkarnanr2018@gmail.com>
2024-03-28 11:08:29 +0530

    varibales

---
+++ variable.js
@@ -0,0 +1,64 @@
+
+
+/*
+ var,
+ let ,
+ const,
+*/
+
+/*
+var =>old varibale 1997 intro
+let,const =>es6 new varibles
+*/
+
+// simple progran
+var a = 10;
+var b = 50;
+var c ;
+c = a+b;
+console.log(c)
+
+
+if(true)
+{
+   var msg = "welcome karnan"
+   console.log(msg)
+}
+console.log(msg)
+
+
+/*
+var keyword using function scope or global scope ,
+it access inside the finction varibale  and accesing outside function varilbel.
+
+1) var key word using reassing and redeclre the varibale
+2)var key using allow hosited varibale
+2)function scope or global scope
+*/
+
+
+
+
+if(true)
+{
+   let msg = "welcome karnan"
+   console.log(msg)
+    msg = "change value"
+    console.log(msg)
+}
+
+
+/*
+let block scope
+ 2) let key using does not allow redeclear value ,but reassign value
+ 2) let cannt accpt hosited varibale
+*/
+
+// const
+/*
+const block scope
+canot reasing vaulue redeclear value
+canot cannt accpt hosited varibale
+*/
+
+ 
