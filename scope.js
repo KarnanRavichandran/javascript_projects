cb62f3fd5937f017197bddf26f53c964940fa3c8
karnan <rkarnanr2018@gmail.com>
2024-03-29 10:55:44 +0530

    function scope

--- scope.js
+++ scope.js
@@ -2,9 +2,23 @@
 
 // excute a block of code 
 
-
+// let and const block scope
 
 if(true){
     let a = 10;
     console.log(a)
 }
+
+// console.log(a)  => referance error
+
+// function js
+// var function scoped
+// declear a varible inside a function can access inside a functiin sope
+ 
+function myFunction(){
+    var name = 'karnan'
+    console.log(name)
+
+}
+
+myFunction()
