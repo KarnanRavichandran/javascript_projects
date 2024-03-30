1e07ed35d23d01966aec988c48dcca7444c5f3c1
karnan <rkarnanr2018@gmail.com>
2024-03-30 11:12:36 +0530

    push

---
+++ push.js
@@ -0,0 +1,10 @@
+let n = [1, 2, 3, 4, 5]
+console.log(n);
+let result = n.push(50,55)
+console.log(n)
+
+// merge array
+let users1 = ["Ram", "Sam", "Ravi"];
+let users2 = ["Rajesh", "Kumar"];
+ users1.push(...users2)
+ console.log(users1)
