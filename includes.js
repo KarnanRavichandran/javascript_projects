8c0911a21f94e673ee1f4d1367ebff74a9b1f6fa
karnan <rkarnanr2018@gmail.com>
2024-03-30 11:09:06 +0530

    include

---
+++ includes.js
@@ -0,0 +1,6 @@
+const products = ["Pen", "Pencil", "Eraser", "Box", "Pen"];
+let result  = products.includes('Pen')
+console.log(result)
+
+result = products.includes("Scale");
+console.log(result);
