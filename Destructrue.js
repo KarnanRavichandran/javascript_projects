d111da690db8cb49cb19074764fefbb3552871cd
karnan <rkarnanr2018@gmail.com>
2024-04-03 10:37:59 +0530

    Destructrue.js

---
+++ Destructrue.js
@@ -0,0 +1,9 @@
+const numbers = [10,20,30,40,50]
+const first_Number = numbers[0]
+const second_Number = numbers[1]
+const third_Number = numbers[2]
+const result = numbers.splice(2)
+console.log(first_Number)
+console.log(second_Number)
+console.log(third_Number)
+console.log(result)
