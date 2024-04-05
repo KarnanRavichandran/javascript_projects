10013041be5a17dee5eb1162160cedce17d0f02d
karnan <rkarnanr2018@gmail.com>
2024-04-05 10:29:51 +0530

    inheritance

--- Class.js
+++ Class.js
@@ -3,8 +3,24 @@ class Person{
         this.name = name;
     }
     sayHello(){
-        console.log(`My name is ${this.name}`)
+        console.log(`My name is ${this.name} ${this.age}`)
     }
 }
-const person = new Person('karnan')
-person.sayHello()
+
+
+// inheritance
+
+class Student extends Person{
+    constructor(name,age){
+        super(name)
+        this.age = age;
+    }
+
+    eligiblity(){
+        console.log(`${this.name} age is a ${this.age}`,this.age >18 ? 'Eligible ' : 'Not Eligible')
+    }
+}
+
+const student = new Student('Ram',12)
+student.sayHello()
+student.eligiblity()
